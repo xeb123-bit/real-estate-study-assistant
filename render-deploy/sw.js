69186b50a9de6a1ef91ec2fbc4b52925659ec842
xeb123-bit <xeb123@gmail.com>
2026-02-17 10:02:13 -0600

    Add files via upload

---
+++ render-deploy/sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 're-study-assistant-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+];
+
+// Install event - cache static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached version or fetch from network
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((fetchResponse) => {
+        // Don't cache API calls or non-GET requests
+        if (
+          !event.request.url.includes('/api/') &&
+          event.request.method === 'GET' &&
+          fetchResponse.status === 200
+        ) {
+          const responseToCache = fetchResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return fetchResponse;
+      });
+    })
+  );
+});
